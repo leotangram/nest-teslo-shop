1d6f7f37d714e42b285602942df2cbdb2155301e
Leonardo Omaña <leotangram@gmail.com>
2023-10-09 16:47:35 -0500

    error manage

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +12,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger(ProductsService.name);
+
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -19,8 +26,7 @@ export class ProductsService {
 
       return product;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Error creating product');
+      this.handleDBExceptions(error);
     }
   }
 
@@ -38,5 +44,12 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Error creating product');
   }
 }
